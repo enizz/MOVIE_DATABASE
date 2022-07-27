37a07b32e97295146305791c4be98d73ab58943a
Eugene <Eugene.Nisnevich@Gmail.com>
2022-07-27 08:14:37 -0400

    Add project files.

---
+++ MOVIE_DATABASE/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOVIE_DATABASE
+{
+    public class Movie
+    {
+        public Movie (string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+    }
+
+
+
+
+
+}
---
+++ MOVIE_DATABASE/Program.cs
@@ -0,0 +1,52 @@
+﻿using MOVIE_DATABASE;
+using System.Linq;
+
+var movies = new List<Movie>
+{
+    new Movie("Dune", "Sci-Fi"),
+    new Movie("The Grey Man", "Action"),
+    new Movie("Tenet", "Thriller"),
+    new Movie("Top Gun: Maverick", "Action"),
+    new Movie("Hustle", "Drama"),
+    new Movie("Snowpiercer", "Sci-Fi"),
+    new Movie("Parasite", "Thriller"),
+    new Movie("Elvis", "Drama"),
+    new Movie("Thor: Love and Thunder", "Comedey"),
+    new Movie("Minions: The Rise of Gru", "Comedey")
+};
+ConsoleKeyInfo key;
+bool answered = false;
+string answer = "";
+Console.WriteLine("Welcome to the Movie List Application!\n");
+Console.WriteLine("There are " + movies.Count() + " movies in this list.");
+
+
+do
+{
+
+
+    do
+    {
+        if (answered == false)
+        {
+            Console.WriteLine("What category are you instereted in?");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, we only have: Sci-Fi, Action, Thriller, Drama or Comedey");
+        }
+        answered = true;
+        answer = Console.ReadLine();
+
+    } while (answer != "Sci-Fi" && answer != "Action" && answer != "Comedey" && answer != "Thriller" && answer != "Drama");
+
+    var categories = movies.Where(x => x.Category == answer);
+
+    foreach (var y in categories)
+    {
+        Console.WriteLine($"{y.Title}");
+    }
+    Console.WriteLine("\nWould you like to search again? If so, press Enter or any other key to exit.");
+    answered = false;
+    key = Console.ReadKey();
+} while (key.Key == ConsoleKey.Enter);
